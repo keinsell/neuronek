4ee531e9b9f9bda3aa6dd43c7ac5c7f53fec5cb9
http:jajek <keinsell@protonmail.com>
2023-01-10 14:44:38 +0100

    test: add unit tests of `Password`

--- apps/server/src/modules/user/value-objects/password/password.spec.ts
+++ apps/server/src/modules/user/value-objects/password/password.spec.ts
@@ -1,5 +1,20 @@
+import { Argon2Hasher } from "@internal/common";
 import ava from "ava";
+import { Password } from "./password.vo.js";
 
-ava.todo(
-  "Password.fromPlain should throw an error if the password is too weak"
+ava(
+  "Password.fromPlain should throw an error if the password is too weak",
+  async (t) => {
+    const password = "password";
+    const hasher = new Argon2Hasher();
+    await t.throwsAsync(Password.fromPlain(password, hasher));
+  }
 );
+
+ava("Password.fromPlain should create a new password", async (t) => {
+  const password = "khjkjasfdsadfsdf";
+  const hasher = new Argon2Hasher();
+  const passwordVO = await Password.fromPlain(password, hasher);
+  t.true(await passwordVO.compare(password));
+  t.false(await passwordVO.compare("wrong password"));
+});
