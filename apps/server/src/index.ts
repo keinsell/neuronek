f8a29651c73bede21fa28900b5f9395e6ab47b6c
http:jajek <keinsell@protonmail.com>
2023-01-10 13:03:18 +0100

    chore: :hammer: improve `nodemon`

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!")
+import { User } from "./modules/user/user.entity.js";
+
+console.log("Hello World!");
+
+console.log(User);
--- apps/server/src/modules/user/user.entity.ts
+++ apps/server/src/modules/user/user.entity.ts
@@ -1,13 +1,20 @@
 import { Entity } from "@internal/common";
+import zxcvbn from "zxcvbn";
 
 export interface UserProperties {
   username: string;
   password: string;
-  recoveryKey: string;
 }
 
 export class User extends Entity<UserProperties> {
-  public static create(props: UserProperties): User {
-    return new User(props);
+  // TODO(NEU-34): https://linear.app/keinsell/issue/NEU-34/user-should-contain-username-which-would-be-automatically-generated
+  // TODO(NEU-33): https://linear.app/keinsell/issue/NEU-33/user-should-contain-cryptographic-recoverykey-which-would-be-based-on
+  public static create(properties: UserProperties): User {
+    // Check strength of password
+    if (zxcvbn(properties.password).score < 3) {
+      throw new Error("Password is too weak");
+    }
+
+    return new User(properties);
   }
 }
