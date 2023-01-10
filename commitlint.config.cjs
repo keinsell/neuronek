eae66f692d17a7c88dc74f2e3fc6081abb9c92ce
http:jajek <keinsell@protonmail.com>
2023-01-10 03:17:46 +0100

    style: disable custom rules for commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,24 +3,4 @@ const defaultConfig = require("@commitlint/config-conventional");
 
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    ...defaultConfig.rules,
-    "type-enum": [
-      2,
-      "always",
-      [
-        "fix",
-        "test",
-        "tooling",
-        "refactor",
-        "revert",
-        "example",
-        "docs",
-        "format",
-        "feat",
-        "chore",
-        "ci",
-      ],
-    ],
-  },
 };
