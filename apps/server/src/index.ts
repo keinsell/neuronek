9a959456458604867da3c5f9aee7da29a11b2f21
http:jajek <keinsell@protonmail.com>
2023-01-10 13:32:21 +0100

    chore: add zxcvbn

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,4 +2,9 @@ import { User } from "./modules/user/user.entity.js";
 
 console.log("Hello World!");
 
-console.log(User);
+console.log(
+  User.create({
+    username: "test",
+    password: "asdkfsdgkadfskj;gldksj",
+  })
+);
