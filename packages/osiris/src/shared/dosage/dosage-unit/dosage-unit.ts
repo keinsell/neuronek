7b167d51a1534c8cdbdc9f5cf4eae04b3a9d48af
http:jajek <keinsell@protonmail.com>
2023-01-19 18:16:59 +0100

    refactor: add `dosage-unit`

---
+++ packages/osiris/src/shared/dosage/dosage-unit/dosage-unit.ts
@@ -0,0 +1,48 @@
+/** Represents single dosage value, for example `30mg` */
+export class DosageUnit {
+	/**
+	 * baseScalar represents highest possible amount of mass (kilograms).
+	 */
+	protected baseScalar: number
+
+	constructor(baseScalar: number) {
+		this.baseScalar = baseScalar
+	}
+
+	/** Converts value to string with attention to `baseScalar` - chooses best possible unit. */
+	toString(): string {
+		if (this.baseScalar < 0.000001) {
+			return `${this.baseScalar / 0.000001}ug`
+		}
+
+		if (this.baseScalar < 0.001) {
+			return `${this.baseScalar / 0.001}mg`
+		}
+
+		if (this.baseScalar < 1) {
+			return `${this.baseScalar}g`
+		}
+
+		return `${this.baseScalar}kg`
+	}
+
+	static fromString(amount: string): DosageUnit {
+		if (amount.endsWith('mg')) {
+			return new DosageUnit(parseFloat(amount) * 0.001)
+		}
+
+		if (amount.endsWith('ug')) {
+			return new DosageUnit(parseFloat(amount) * 0.000001)
+		}
+
+		if (amount.endsWith('g')) {
+			return new DosageUnit(parseFloat(amount))
+		}
+
+		if (amount.endsWith('kg')) {
+			return new DosageUnit(parseFloat(amount) * 1000)
+		}
+
+		throw new Error(`Invalid dosage unit provided: ${amount}`)
+	}
+}
