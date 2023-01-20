9cb135e6e29ef472eabb26c737203475af28ff94
http:jajek <keinsell@protonmail.com>
2023-01-20 23:51:14 +0100

    feat(`osiris`): add `RouteOfAdministration` (#246)

---
+++ packages/osiris/src/shared/route-of-administration/route-of-administration.spec.ts
@@ -0,0 +1,16 @@
+import test from 'ava'
+import { RouteOfAdministration } from './route-of-administration.js'
+import { RouteOfAdministrationClassification } from './route-of-administration-classification.js'
+import { DosageTable } from '../dosage/dosage-table/dosage-table.js'
+import { PhaseTable } from '../phase/phase-table/phase-table.js'
+
+test('constructor(): should create route of administration', t => {
+	const routeOfAdministration = new RouteOfAdministration({
+		classification: RouteOfAdministrationClassification.insufflated,
+		dosage: new DosageTable({}),
+		phase: new PhaseTable({})
+	})
+	t.is(routeOfAdministration.classification.toString(), 'insufflated')
+	t.deepEqual(routeOfAdministration.dosage, new DosageTable({}))
+	t.deepEqual(routeOfAdministration.phase, new PhaseTable({}))
+})
