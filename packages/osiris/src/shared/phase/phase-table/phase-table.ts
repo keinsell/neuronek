1c1eaad23db4fe6abcad67cd339168e55538ac34
http:jajek <keinsell@protonmail.com>
2023-01-20 23:33:39 +0100

    feat(`osiris`): add `PhaseTable` (#244)

---
+++ packages/osiris/src/shared/phase/phase-table/phase-table.ts
@@ -0,0 +1,82 @@
+import { PhaseClassification } from '../phase-classification.js'
+import { Phase } from '../phase.js'
+
+export class PhaseTable {
+	public readonly [PhaseClassification.onset]?: Phase
+	public readonly [PhaseClassification.comeup]?: Phase
+	public readonly [PhaseClassification.peak]?: Phase
+	public readonly [PhaseClassification.offset]?: Phase
+	public readonly [PhaseClassification.aftereffects]?: Phase
+
+	/** Represents order of phases, starting up at `onset` and ending at `aftereffects` */
+	protected readonly phaseFunnel = [
+		PhaseClassification.onset,
+		PhaseClassification.comeup,
+		PhaseClassification.peak,
+		PhaseClassification.offset,
+		PhaseClassification.aftereffects
+	]
+
+	constructor(
+		phaseTable: Partial<{
+			[key in PhaseClassification]: Phase
+		}>
+	) {
+		this[PhaseClassification.onset] = phaseTable.onset
+		this[PhaseClassification.comeup] = phaseTable.comeup
+		this[PhaseClassification.peak] = phaseTable.peak
+		this[PhaseClassification.offset] = phaseTable.offset
+		this[PhaseClassification.aftereffects] = phaseTable.aftereffects
+	}
+
+	/** Get total duration of effects, this excludes aftereffects. */
+	get totalDuration(): Phase {
+		const phasesConsideredForTotalDuration: Phase[] = [
+			this[PhaseClassification.onset],
+			this[PhaseClassification.comeup],
+			this[PhaseClassification.peak],
+			this[PhaseClassification.offset]
+		]
+
+		// Calculate minimal duration of effects
+		const minimalDuration = phasesConsideredForTotalDuration
+			.map(phase => phase?.minimalDuration)
+			.reduce((acc, curr) => (acc ?? 0) + (curr ?? 0), 0)
+
+		// Calculate maximal duration of effects
+		const maximalDuration = phasesConsideredForTotalDuration
+			.map(phase => phase?.maximalDuration)
+			.reduce((acc, curr) => (acc ?? 0) + (curr ?? 0), 0)
+
+		// Create Phase instance
+		return new Phase({ minimalDuration, maximalDuration })
+	}
+
+	/** Get time needed to get into specific phase */
+	calculateTimeToStartOfPhase(phase: PhaseClassification): Phase {
+		let minimalTime = 0
+		let maximalTime = 0
+
+		for (const funnelPhase of this.phaseFunnel) {
+			if (funnelPhase === phase) break
+
+			minimalTime += this[funnelPhase]?.minimalDuration ?? 0
+			maximalTime += this[funnelPhase]?.maximalDuration ?? 0
+		}
+
+		return new Phase({ minimalDuration: minimalTime, maximalDuration: maximalTime })
+	}
+
+	/** Get time needed to get out of specific phase */
+	calculateTimeToEndOfPhase(phase: PhaseClassification): Phase {
+		const timeToStartOfPhase = this.calculateTimeToStartOfPhase(phase)
+		const durationOfPhase = this[phase]
+
+		const timeToEndOfPhase = new Phase({
+			minimalDuration: timeToStartOfPhase.minimalDuration + (durationOfPhase?.minimalDuration ?? 0),
+			maximalDuration: timeToStartOfPhase.maximalDuration + (durationOfPhase?.maximalDuration ?? 0)
+		})
+
+		return timeToEndOfPhase
+	}
+}
--- packages/osiris/src/shared/route-of-administration/route-of-administration.ts
+++ packages/osiris/src/shared/route-of-administration/route-of-administration.ts
@@ -1,8 +1,10 @@
 import { DosageTable } from '../dosage/dosage-table/dosage-table.js'
+import { PhaseTable } from '../phase/phase-table/phase-table.js'
 import { RouteOfAdministrationClassification } from './route-of-administration-classification.js'
 
 export class RouteOfAdministration {
 	classification: RouteOfAdministrationClassification
 	bioavailability?: number
 	dosage: DosageTable
+	phase: PhaseTable
 }
