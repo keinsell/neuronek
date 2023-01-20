ad420e06a8f50165180d4903361277083e83448a
http:jajek <keinsell@protonmail.com>
2023-01-20 21:16:28 +0100

    feat(`osiris`): add `DosageTable` (#240)

--- packages/osiris/src/shared/dosage/dosage-table/dosage-table.ts
+++ packages/osiris/src/shared/dosage/dosage-table/dosage-table.ts
@@ -5,11 +5,51 @@ import { DosageUnit } from '../dosage-unit/dosage-unit.js'
  * DosageTable is a class that holds infomration about dosage classification for specific substance or route of administration.
  */
 export class DosageTable {
-	protected _table: {
-		[key in DosageClassification]?: DosageUnit
+	public readonly [DosageClassification.thereshold]?: DosageUnit
+	public readonly [DosageClassification.light]?: DosageUnit
+	public readonly [DosageClassification.moderate]?: DosageUnit
+	public readonly [DosageClassification.strong]?: DosageUnit
+	public readonly [DosageClassification.heavy]?: DosageUnit
+
+	constructor(
+		dosageTable: Partial<{
+			[key in DosageClassification]: DosageUnit
+		}>
+	) {
+		this[DosageClassification.thereshold] = dosageTable.thereshold
+		this[DosageClassification.light] = dosageTable.light
+		this[DosageClassification.moderate] = dosageTable.moderate
+		this[DosageClassification.strong] = dosageTable.strong
+		this[DosageClassification.heavy] = dosageTable.heavy
 	}
 
+	/** Method will compare provided dosage unit with available dosage table to find classification of provided dosage. */
 	public getClassificationOfDosage(dosage: DosageUnit): DosageClassification {
-		throw Error('Not implemented')
+		if (this[DosageClassification.heavy] && dosage.gte(this[DosageClassification.heavy]))
+			return DosageClassification.heavy
+
+		if (
+			this[DosageClassification.strong] &&
+			dosage.gte(this[DosageClassification.strong]) &&
+			dosage.lt(this[DosageClassification.heavy])
+		)
+			return DosageClassification.strong
+
+		if (
+			this[DosageClassification.moderate] &&
+			dosage.gte(this[DosageClassification.moderate]) &&
+			dosage.lt(this[DosageClassification.strong])
+		)
+			return DosageClassification.moderate
+
+		if (
+			this[DosageClassification.light] &&
+			dosage.gte(this[DosageClassification.light]) &&
+			dosage.lt(this[DosageClassification.moderate])
+		)
+			return DosageClassification.light
+
+		if (this[DosageClassification.thereshold] && dosage.lte(this[DosageClassification.thereshold]))
+			return DosageClassification.thereshold
 	}
 }
