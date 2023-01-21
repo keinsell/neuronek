8755f4185e1b2a837d6838eba7f04ed52dbbbc08
http:jajek <keinsell@protonmail.com>
2023-01-21 04:45:00 +0100

    chore: export default osiris

--- packages/osiris/src/index.ts
+++ packages/osiris/src/index.ts
@@ -1,7 +1,9 @@
 import { Osiris } from './osiris.js'
 import { getSubstanceFromPsychonautWiki } from './providers/psychonautwiki/get-substance/get-substance.js'
 
-export const osiris = new Osiris()
+const osiris = new Osiris()
 
 export { DosageClassification } from './shared/dosage/dosage-classification.js'
 export { RouteOfAdministrationClassification } from './shared/route-of-administration/route-of-administration-classification.js'
+
+export default osiris
