f903e2fb80f2c467200997af5c21ecd0f3f73fe1
http:jajek <keinsell@protonmail.com>
2023-01-21 04:44:48 +0100

    feat(`osiris`): add `findSubstanceByName`

--- packages/osiris/src/osiris.ts
+++ packages/osiris/src/osiris.ts
@@ -1,6 +1,29 @@
+import { getSubstanceFromPsychonautWiki } from './providers/psychonautwiki/get-substance/get-substance.js'
 import { Substance } from './shared/substance/substance.js'
 
 export class Osiris {
-	// async findSubstancesBy(filters: {effect?: string, psychoactiveClass?: string, chemicalClass?: string, name?: string}): Promise<Substance[]> {}
+	async findSubstancesBy(filters: {
+		effect?: string
+		psychoactiveClass?: string
+		chemicalClass?: string
+		name?: string
+	}): Promise<Substance[]> {
+		// Find substance by PsychonautWiki
+		if (filters.name) {
+			const psychonautWikiSubstance = await getSubstanceFromPsychonautWiki(filters.name)
+			if (psychonautWikiSubstance) {
+				return [psychonautWikiSubstance]
+			}
+		}
+
+		return []
+	}
+
+	async findSubstanceByName(substanceName: string): Promise<Substance | undefined> {
+		// Find substance by PsychonautWiki
+		const psychonautWikiSubstance = await getSubstanceFromPsychonautWiki(substanceName)
+		return psychonautWikiSubstance
+	}
+
 	// async findSubstancesByRouteOfAdministrationClassification(roa: string): Promise<Substance[]> {}
 }
