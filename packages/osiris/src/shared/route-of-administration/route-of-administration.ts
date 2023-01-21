6e1ff90a5c862f20b4f47c26a17551c1f2729619
http:jajek <keinsell@protonmail.com>
2023-01-21 01:05:18 +0100

    feat: add `RouteOfAdministrationTable`

--- packages/osiris/src/shared/route-of-administration/route-of-administration.ts
+++ packages/osiris/src/shared/route-of-administration/route-of-administration.ts
@@ -1,20 +1,12 @@
 import { DosageTable } from '../dosage/dosage-table/dosage-table.js'
 import { PhaseTable } from '../phase/phase-table/phase-table.js'
-import { RouteOfAdministrationClassification } from './route-of-administration-classification.js'
 
 export class RouteOfAdministration {
-	public readonly classification: RouteOfAdministrationClassification
 	public readonly bioavailability?: number
 	public readonly dosage: DosageTable
 	public readonly phase: PhaseTable
 
-	constructor(administrationRoute: {
-		classification: RouteOfAdministrationClassification
-		bioavailability?: number
-		dosage: DosageTable
-		phase: PhaseTable
-	}) {
-		this.classification = administrationRoute.classification
+	constructor(administrationRoute: { bioavailability?: number; dosage: DosageTable; phase: PhaseTable }) {
 		this.bioavailability = administrationRoute.bioavailability
 		this.dosage = administrationRoute.dosage
 		this.phase = administrationRoute.phase
