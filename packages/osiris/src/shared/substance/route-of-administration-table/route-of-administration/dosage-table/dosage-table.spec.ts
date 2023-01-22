8729315ca01d76ef0f64955fecabec17cd772196
http:jajek <keinsell@protonmail.com>
2023-01-22 10:14:33 +0100

    test: use `test.before` for `DosageTable`

--- packages/osiris/src/shared/substance/route-of-administration-table/route-of-administration/dosage-table/dosage-table.spec.ts
+++ packages/osiris/src/shared/substance/route-of-administration-table/route-of-administration/dosage-table/dosage-table.spec.ts
@@ -1,67 +1,58 @@
-import test from 'ava'
+import ava, { TestFn } from 'ava'
 import { DosageClassification } from './dosage-classification.js'
 import { DosageUnit } from './dosage-unit/dosage-unit.js'
 import { DosageTable } from './dosage-table.js'
 
-test('getClassificationOfDosage(): should return dosage classification', t => {
-	const dosageTable = new DosageTable({
+const test = ava as TestFn<{
+	dosageTable: {
+		[dosage in DosageClassification]: DosageUnit
+	}
+}>
+
+test.beforeEach(t => {
+	t.context.dosageTable = {
 		thereshold: new DosageUnit(0.5, 'mg'),
 		light: new DosageUnit(0.75, 'mg'),
 		moderate: new DosageUnit(1, 'mg'),
 		strong: new DosageUnit(1.5, 'mg'),
 		heavy: new DosageUnit(2, 'mg')
-	})
+	}
+})
 
-	const expectedClassifications: { dosage: DosageUnit; expectedClassification: DosageClassification }[] = [
-		{ dosage: new DosageUnit(0.25, 'mg'), expectedClassification: DosageClassification.thereshold },
-		{ dosage: new DosageUnit(0.5, 'mg'), expectedClassification: DosageClassification.thereshold },
-		{ dosage: new DosageUnit(0.75, 'mg'), expectedClassification: DosageClassification.light },
-		{ dosage: new DosageUnit(1, 'mg'), expectedClassification: DosageClassification.moderate },
-		{ dosage: new DosageUnit(1.5, 'mg'), expectedClassification: DosageClassification.strong },
-		{ dosage: new DosageUnit(2, 'mg'), expectedClassification: DosageClassification.heavy },
-		{ dosage: new DosageUnit(2.5, 'mg'), expectedClassification: DosageClassification.heavy }
-	]
+test('getClassificationOfDosage(): should return dosage classification', t => {
+	const dosageTable = new DosageTable(t.context.dosageTable)
 
-	for (const { dosage, expectedClassification } of expectedClassifications) {
+	for (const value of Object.values(DosageClassification)) {
+		const dosage = t.context.dosageTable[value]
+
 		t.is(
 			dosageTable.getClassificationOfDosage(dosage),
-			expectedClassification,
-			`Dosage ${dosage.toString()} should be ${expectedClassification} according to table.`
+			value,
+			`Dosage classification of ${dosage.toString()} should be ${value}`
 		)
 	}
 })
 
 test('toJSON(): should return JSONed data', t => {
-	const dosageTable = new DosageTable({
-		thereshold: new DosageUnit(0.5, 'mg'),
-		light: new DosageUnit(0.75, 'mg'),
-		moderate: new DosageUnit(1, 'mg'),
-		strong: new DosageUnit(1.5, 'mg'),
-		heavy: new DosageUnit(2, 'mg')
-	})
+	const dosageTable = new DosageTable(t.context.dosageTable)
 
-	t.deepEqual(dosageTable.toJSON(), {
-		thereshold: '0.5 mg',
-		light: '0.75 mg',
-		moderate: '1 mg',
-		strong: '1.5 mg',
-		heavy: '2 mg'
-	})
+	const exprected = {}
+
+	for (const classification of Object.keys(DosageClassification)) {
+		exprected[classification] = t.context.dosageTable[classification]?.toString()
+	}
+
+	t.deepEqual(dosageTable.toJSON(), exprected)
 })
 
 test('fromJSON(): should return dosage table from JSON', t => {
-	const dosageTable = DosageTable.fromJSON({
-		thereshold: '0.5 mg',
-		light: '0.75 mg',
-		moderate: '1 mg',
-		strong: '1.5 mg'
-	})
+	const json = {}
 
-	t.deepEqual(dosageTable.toJSON(), {
-		thereshold: '0.5 mg',
-		light: '0.75 mg',
-		moderate: '1 mg',
-		strong: '1.5 mg',
-		heavy: undefined
-	})
+	for (const classification of Object.keys(DosageClassification)) {
+		json[classification] = t.context.dosageTable[classification]?.toString()
+	}
+
+	const dosageTable = DosageTable.fromJSON(json)
+
+	t.deepEqual(dosageTable.toJSON(), json)
 })
