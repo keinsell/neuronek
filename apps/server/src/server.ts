b8b1e5e1e81914979347a40e0c98c36cad6622a9
http:jajek <keinsell@protonmail.com>
2023-01-23 03:18:02 +0100

    refactor: remove `UserRepository`

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,8 +2,6 @@ import express, { Request, Response, json, urlencoded } from 'express'
 import { RegisterRoutes } from '../dist/routes'
 import openapi from '../dist/swagger.json'
 import swaggerUI from 'swagger-ui-express'
-import { container } from './infrastructure/ioc/container.js'
-import { UserRepository } from './modules/user/user.repository.js'
 
 export const app = express()
 
@@ -17,10 +15,6 @@ app.use(json())
 app.use('/docs', swaggerUI.serve, (_req: Request, res: Response) => {
 	return res.send(swaggerUI.generateHTML(openapi))
 })
-
-const us = container.get(UserRepository)
-
-console.log(await us.findUserById('adsadfsdf'))
 
 RegisterRoutes(app)
 
