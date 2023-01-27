f0eb0660990635861fc8be21a665424f05ac1375
http:jajek <keinsell@protonmail.com>
2023-01-28 00:46:08 +0100

    feat: add `PhaseParagraphProperties` (#289)

--- packages/osiris/src/shared/experience-report/experience-report.ts
+++ packages/osiris/src/shared/experience-report/experience-report.ts
@@ -1,4 +1,6 @@
+import { Effect } from '../effect/effect.js'
 import { IngestionProperties } from '../ingestion/ingestion.js'
+
 export interface SubjectProperties {
 	name?: string
 	gender?: string
@@ -8,4 +10,10 @@ export interface SubjectProperties {
 	height?: number
 	medications?: string[]
 	mental_illness?: string[]
+}
+
+export interface PhaseParagraphProperties {
+	time?: string
+	content: string[]
+	effects?: Effect[]
 }
