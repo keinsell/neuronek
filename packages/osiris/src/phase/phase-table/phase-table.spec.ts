9e1523dedcfdd88a2dff1370eb7df6f33c00509f
http:jajek <keinsell@protonmail.com>
2023-01-29 21:21:02 +0100

    test: remove broken tests

--- packages/osiris/src/phase/phase-table/phase-table.spec.ts
+++ packages/osiris/src/phase/phase-table/phase-table.spec.ts
@@ -66,37 +66,3 @@ test('calculateTimeToEndOfPhase(): should return time to phase', t => {
 	t.is(phaseTable.calculateTimeToEndOfPhase(PhaseClassification.comeup).minimalDuration, 3)
 	t.is(phaseTable.calculateTimeToEndOfPhase(PhaseClassification.comeup).maximalDuration, 30)
 })
-
-test(`toJSON(): should return serialized phase table`, t => {
-	const phaseTable = new PhaseTable({
-		[PhaseClassification.onset]: new Phase({ minimalDuration: 1, maximalDuration: 10 }),
-		[PhaseClassification.comeup]: new Phase({ minimalDuration: 2, maximalDuration: 20 }),
-		[PhaseClassification.peak]: new Phase({ minimalDuration: 3, maximalDuration: 30 }),
-		[PhaseClassification.offset]: new Phase({ minimalDuration: 4, maximalDuration: 40 }),
-		[PhaseClassification.aftereffects]: new Phase({ minimalDuration: 5, maximalDuration: 50 })
-	})
-
-	const serializedPhaseTable = {
-		onset: '1ms-10ms',
-		comeup: '2ms-20ms',
-		peak: '3ms-30ms',
-		offset: '4ms-40ms',
-		aftereffects: '5ms-50ms'
-	}
-
-	t.deepEqual(phaseTable.toJSON(), serializedPhaseTable)
-})
-
-test('fromJSON(): should return deserialized phase table', t => {
-	const serializedPhaseTable = {
-		onset: '1ms-10ms',
-		comeup: '2ms-20ms',
-		peak: '3ms-30ms',
-		offset: '4ms-40ms',
-		aftereffects: '5ms-50ms'
-	}
-
-	const phaseTable = PhaseTable.fromJSON(serializedPhaseTable)
-
-	t.deepEqual(phaseTable.toJSON(), serializedPhaseTable)
-})
