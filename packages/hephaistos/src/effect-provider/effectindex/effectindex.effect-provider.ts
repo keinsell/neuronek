2d6bb149886647c049672e4a12205edec06e0f2c
http:jajek <keinsell@protonmail.com>
2023-01-31 22:54:10 +0100

    refactor(`effectindex`): add mappings for broken titles

--- packages/hephaistos/src/effect-provider/effectindex/effectindex.effect-provider.ts
+++ packages/hephaistos/src/effect-provider/effectindex/effectindex.effect-provider.ts
@@ -5,11 +5,24 @@ import { EffectProviderAdapter } from '../effect-provider.adapter.js'
 
 class EffectIndexMapper {
 	static toDomain(effect: ParsedPage): Effect {
-		const title = effect.title
+		let title = effect.title
 		const summary = effect.description
 		const raw_description = effect.text.split('\n')
 		const url = effect.url
 		const description = []
+
+		// TODO: Move these mapping to effectindex-dataset package?
+
+		title === 'An' ? (title = 'Epileptic seizure') : title
+		title === 'Watery' ? (title = 'Watery eyes') : title
+		title === 'Visual' ? (title = 'Visual haze') : title
+		title === 'Pain' ? (title = 'Pain relief') : title
+		title === 'Ego' ? (title = 'Ego death') : title
+		title === 'Dry' ? (title = 'Dry mouth') : title
+		title === 'A' ? (title = 'Runny nose') : title
+		title === 'Déjà' ? (title = 'Déjà Vu') : title
+		title === 'Brain' ? (title = 'Brain zaps') : title
+		title === 'Back' ? (title = 'Back pain') : title
 
 		for (const line of raw_description) {
 			if (line.startsWith('[')) continue
