ee9b18c1a2cea2bd00bcbc64d0ed7e4b5dda4cae
http:jajek <keinsell@protonmail.com>
2023-01-31 22:36:22 +0100

    refactor(`effectindex`): add `EffectIndexMapper`

--- packages/hephaistos/src/effect-provider/effectindex/effectindex.effect-provider.ts
+++ packages/hephaistos/src/effect-provider/effectindex/effectindex.effect-provider.ts
@@ -1,33 +1,38 @@
-import effectindex from 'effectindex-dataset'
+import effectindex, { ParsedPage } from 'effectindex-dataset'
 import { Effect } from 'osiris'
 
 import { EffectProviderAdapter } from '../effect-provider.adapter.js'
 
+class EffectIndexMapper {
+	static toDomain(effect: ParsedPage): Effect {
+		const title = effect.title
+		const summary = effect.description
+		const raw_description = effect.text.split('\n')
+		const url = effect.url
+		const description = []
+
+		for (const line of raw_description) {
+			if (line.startsWith('[')) continue
+			if (line === '') continue
+
+			description.push(line)
+		}
+		return new Effect({ name: title, summary: summary, description: description, effectindex: url })
+	}
+}
+
 export class EffectIndexEffectProvider extends EffectProviderAdapter {
 	async findByName(name: string): Promise<Effect> {
 		const effect = effectindex.find((effect: { title: string }): boolean => effect.title === name)
-		return new Effect({ name: effect.title })
+
+		return EffectIndexMapper.toDomain(effect)
 	}
 
 	async all(): Promise<Effect[]> {
 		const effects: Effect[] = []
 
 		for (const effect of effectindex) {
-			const title = effect.title
-			const summary = effect.description
-			const raw_description = effect.text.split('\n')
-			// Descroption sometimes contains an empty string and [1], [2] etc..., we should clean that up 🧻
-
-			const description = []
-
-			for (const line of raw_description) {
-				if (line.startsWith('[')) continue
-				if (line === '') continue
-
-				description.push(line)
-			}
-
-			effects.push(new Effect({ name: title, summary: summary, description: description }))
+			effects.push(EffectIndexMapper.toDomain(effect))
 		}
 
 		return effects
