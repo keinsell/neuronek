367b728737274d3a7e9548d98fb6d4ba7f58c6a9
http:jajek <keinsell@protonmail.com>
2023-02-04 18:41:53 +0100

    refactor: `Ingestion`

--- packages/osiris/src/ingestion/ingestion.ts
+++ packages/osiris/src/ingestion/ingestion.ts
@@ -2,23 +2,7 @@ import { Dosage } from '../dosage/dosage.js'
 import { RouteOfAdministrationClassification } from '../route-of-administration/RouteOfAdministrationClassification.js'
 import { Substance } from '../substance/substance.js'
 
-export interface IngestionProperties {
-	substance?: Substance
-	dosage?: Dosage
-	routeOfAdministration?: RouteOfAdministrationClassification
-	// TODO: Should this be refactored into Dosage (or maybe separate Dosage class)?
-	purity?: number
-	// TODO: Should this be refactored into Dosage (or maybe separate Dosage class)?
-	/** This field should be marked if subject isn't sure about measured dosage, thus this should not be taken in exact calculations. */
-	isDosageEstimate?: boolean
-	// TODO: Add forms of substances ex. powder, liquid, capsule, gel, etc.
-	form?: string
-	date?: Date
-	location?: string
-	notes?: string
-}
-
-export class Ingestion implements IngestionProperties {
+export class Ingestion {
 	substance?: Substance
 	dosage?: Dosage
 	routeOfAdministration?: RouteOfAdministrationClassification
@@ -33,7 +17,23 @@ export class Ingestion implements IngestionProperties {
 	location?: string
 	notes?: string
 
-	constructor(payload: IngestionProperties) {
-		Object.assign(this, payload)
+	private constructor(properties: {
+		substance?: Substance
+		dosage?: Dosage
+		routeOfAdministration?: RouteOfAdministrationClassification
+		purity?: number
+		isDosageEstimate?: boolean
+	}) {
+		Object.assign(this, properties)
+	}
+
+	static create(properties: {
+		substance?: Substance
+		dosage?: Dosage
+		routeOfAdministration?: RouteOfAdministrationClassification
+		purity?: number
+		isDosageEstimate?: boolean
+	}): Ingestion {
+		return new Ingestion(properties)
 	}
 }
