43e7208679f5abb3d25a8fe64e4e6e0acb1e0d39
http:jajek <keinsell@protonmail.com>
2023-02-04 18:00:18 +0100

    refactor: `Substance`

--- packages/osiris/src/substance/substance.ts
+++ packages/osiris/src/substance/substance.ts
@@ -1,29 +1,59 @@
-import { ValueObject } from '../__core/valueobject.js'
 import { RouteOfAdministrationTable } from '../route-of-administration/route-of-administration-table.js'
 import { ChemicalNomenclature } from './chemical-nomenclature/chemical-nomenclature'
 import { ClassMembership } from './class-membership/class-membership.js'
 import { ExternalReferenceTable } from './external-reference-table/external-reference-table.js'
 
-export interface SubstanceProperites {
+export class Substance {
 	/**
 	 * Most popular common name for the substance.
 	 * @example "Amphetamine"
 	 */
-	name: string
+	public readonly name: string
+
 	/**
 	 * Chemical nomenclature is the system of naming chemical compounds. The rules for naming compounds vary depending on the type of compound, but in general, they are based on the type and number of atoms present in the compound, as well as the chemical bonds between them. The most common system of chemical nomenclature is the International Union of Pure and Applied Chemistry (IUPAC) system, which is widely used in scientific literature and in industry.
 	 */
-	nomenclature?: ChemicalNomenclature
+	public readonly nomenclature?: ChemicalNomenclature
+
 	/**
 	 * Class membership refers to the classification of a chemical compound based on its structural and/or functional properties. In chemistry, compounds are often grouped into classes based on their chemical characteristics, such as their chemical formula, functional groups, or reactivity.
 	 */
-	class_membership?: ClassMembership
+	public readonly class_membership?: ClassMembership
+
 	/**
 	 * Routes of administration refer to the different ways in which a chemical compound or a drug can be taken into the body.
 	 */
-	routes_of_administration?: RouteOfAdministrationTable
+	public readonly routes_of_administration?: RouteOfAdministrationTable
 
-	externals?: ExternalReferenceTable
+	public readonly externals?: ExternalReferenceTable
+
+	private constructor(
+		name: string,
+		nomenclature?: ChemicalNomenclature,
+		class_membership?: ClassMembership,
+		routes_of_administration?: RouteOfAdministrationTable,
+		externals?: ExternalReferenceTable
+	) {
+		this.name = name
+		this.nomenclature = nomenclature
+		this.class_membership = class_membership
+		this.routes_of_administration = routes_of_administration
+		this.externals = externals
+	}
+
+	public static create(payload: {
+		name: string
+		nomenclature?: ChemicalNomenclature
+		class_membership?: ClassMembership
+		routes_of_administration?: RouteOfAdministrationTable
+		externals?: ExternalReferenceTable
+	}): Substance {
+		return new Substance(
+			payload.name,
+			payload.nomenclature,
+			payload.class_membership,
+			payload.routes_of_administration,
+			payload.externals
+		)
+	}
 }
-
-export class Substance extends ValueObject<SubstanceProperites> {}
