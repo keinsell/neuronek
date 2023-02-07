83c94b7254a58a93bdf13872eae167fbabe92f5c
http:jajek <keinsell@protonmail.com>
2023-02-07 03:02:49 +0100

    refactor(`Ingestion`): add `Subject` to ingestion

--- packages/osiris/src/ingestion/ingestion.ts
+++ packages/osiris/src/ingestion/ingestion.ts
@@ -1,53 +1,51 @@
 import { Dosage } from '../dosage/dosage.js'
 import { Mixture } from '../mixtures/mixture.model.js'
 import { RouteOfAdministrationClassification } from '../route-of-administration/RouteOfAdministrationClassification.js'
+import { Subject } from '../subject/subject.js'
 import { Substance } from '../substance/substance.js'
 
-export class Ingestion {
-	substance?: Substance
-	mixture?: Mixture
-	dosage?: Dosage
+export interface IngestionProperties {
+	substance_name?: string
+	mixture_id?: string
 	routeOfAdministration?: RouteOfAdministrationClassification
-	// TODO: Should this be refactored into Dosage (or maybe separate Dosage class)?
-	purity?: number
-	// TODO: Should this be refactored into Dosage (or maybe separate Dosage class)?
-	/** This field should be marked if subject isn't sure about measured dosage, thus this should not be taken in exact calculations. */
-	isDosageEstimate?: boolean
-	// TODO: Add forms of substances ex. powder, liquid, capsule, gel, etc.
-	form?: string
+	dosage?: Dosage
+	Substance?: Substance
+	Mixture?: Mixture
+	date?: Date
+	location?: string
+	notes?: string
+	subject_username: string
+	Subject?: Subject
+}
+
+export class Ingestion {
+	substance_name?: string
+	mixture_id?: string
+	routeOfAdministration?: RouteOfAdministrationClassification
+	dosage?: Dosage
+	Substance?: Substance
+	Mixture?: Mixture
 	date?: Date
 	location?: string
 	notes?: string
 
-	private constructor(properties: {
-		substance?: Substance
-		dosage?: Dosage
-		routeOfAdministration?: RouteOfAdministrationClassification
-		purity?: number
-		isDosageEstimate?: boolean
-	}) {
+	private constructor(properties: IngestionProperties) {
 		Object.assign(this, properties)
 	}
 
 	// eslint-disable-next-line jsdoc/require-returns
 	/** Checks if ingestion contains information about ingested substance or mixture. If contains informations about mixture additionally checks if mixture contains information about it's content. */
 	public hasKnownSubstance(): boolean {
-		let hasSubstance = !!this.substance
+		let hasSubstance = !!this.Substance
 
-		if (!hasSubstance && this.mixture) {
-			hasSubstance = this.mixture.ingredients?.some(ingredient => !!ingredient.substance_name)
+		if (!hasSubstance && this.Mixture) {
+			hasSubstance = this.Mixture.ingredients?.some(ingredient => !!ingredient.substance_name)
 		}
 
 		return hasSubstance
 	}
 
-	static create(properties: {
-		substance?: Substance
-		dosage?: Dosage
-		routeOfAdministration?: RouteOfAdministrationClassification
-		purity?: number
-		isDosageEstimate?: boolean
-	}): Ingestion {
+	static create(properties: IngestionProperties): Ingestion {
 		return new Ingestion(properties)
 	}
 }
