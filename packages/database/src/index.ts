ba3f34b489c6e68c12864a36398d5fdba3827962
http:jajek <keinsell@protonmail.com>
2023-02-18 21:42:52 +0100

    refactor(database): remove useless repositories

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from '@prisma/client'
-export { EffectRepository } from './context.effect/effect.repository.js'
-
-// declare global {
-// 	var prisma: PrismaClient | undefined
-// }
-
-// export const prisma = global.prisma || new PrismaClient()
-
-// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export { PrismaClient }
