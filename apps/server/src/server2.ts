29585c1ebc62759634854b47856589b6b35032fa
Jakub Olan <keinsell@protonmail.com>
2023-04-02 01:41:53 +0200

    feat: add defining new auth challange

--- apps/server/src/server2.ts
+++ apps/server/src/server2.ts
@@ -2,6 +2,7 @@ import { App } from '@tinyhttp/app'
 import { json } from 'milliparsec'
 import { PrismaClient } from '@prisma/client'
 import { createAccount } from './features/account/create-account/create-account.controller.js'
+import { defineNewAuthChallenge } from './features/authorization/create-authorization-challenge/createAuthChallenge.js'
 
 const prisma = new PrismaClient()
 const app = new App()
@@ -13,6 +14,10 @@ app.get('/', (req, res) => res.send('Hello World'))
 
 app.post('/account', async (req, res) => {
 	return await createAccount(req, res)
+})
+
+app.get('/challange/:username', async (req, res) => {
+	return await defineNewAuthChallenge(req, res)
 })
 
 app.listen(666, () => {
