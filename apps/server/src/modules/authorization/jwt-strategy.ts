5e03513691a69026e14073dd83d5cd78b90f3eda
Jakub Olan <keinsell@protonmail.com>
2023-05-10 23:37:43 +0200

    chore: saf

--- apps/server/src/modules/authorization/jwt-strategy.ts
+++ apps/server/src/modules/authorization/jwt-strategy.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt'
+import { AccessToken } from './domain/value-objects/access-token'
+import { RefreshToken } from './domain/value-objects/refresh-token'
 
 const prisma = new PrismaClient()
 
@@ -10,17 +12,18 @@ const options: StrategyOptions = {
 
 export class JwtStrategy extends Strategy {
 	constructor() {
-		super(options, async (payload, done) => {
+		super(options, async (payload: RefreshToken | AccessToken, done) => {
+			// Find user in database.
 			const user = await prisma.account.findUnique({ where: { id: payload.sub } })
 
-			if (!user) {
-				return done(null, false)
-			}
+			// TODO: Find session in database and check if it is valid.
 
 			if (user) {
 				return done(null, user)
 			}
 
+			// TODO: Update last logged in date in Sessions.
+			this.success(user)
 			return done(null, false)
 		})
 	}
