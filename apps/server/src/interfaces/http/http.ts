169901ee3560f324abfa5a76bd3c35c33f7d04d4
keinsell <keinsell@protonmail.com>
2023-05-13 20:46:06 +0200

    fix: 🐳 server image do not see `node_modules` (#366)

--- apps/server/src/interfaces/http/http.ts
+++ apps/server/src/interfaces/http/http.ts
@@ -4,7 +4,7 @@ import * as Tracing from '@sentry/tracing'
 import express, { Application } from 'express'
 import { json } from 'milliparsec'
 import { prisma } from '../../shared/infrastructure/prisma/prisma.js'
-import { createAccount } from './routes/account/post.js'
+import { createAccount } from './routes/account/account.post.js'
 import { defineNewAuthChallenge } from './routes/challange/[username]/challange.[username].get.js'
 import { solveAuthChallenge } from './routes/challange/challange.post.js'
 
