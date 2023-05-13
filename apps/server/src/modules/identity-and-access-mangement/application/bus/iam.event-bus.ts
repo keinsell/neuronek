8744d6f75ba343c0e8c6b6ce46cd478166c1927a
keinsell <keinsell@protonmail.com>
2023-05-13 20:52:19 +0200

    refactor(cqrs): ♻️ rename `Event` to `SimpleEvent` (#367)

--- apps/server/src/modules/identity-and-access-mangement/application/bus/iam.event-bus.ts
+++ apps/server/src/modules/identity-and-access-mangement/application/bus/iam.event-bus.ts
@@ -1,6 +1,6 @@
-import { Event } from '../../../../shared/core/cqrs/event/event'
 import { EventBinding } from '../../../../shared/core/cqrs/event/event-binding'
 import { EventBus } from '../../../../shared/core/cqrs/event/event-bus'
+import { SimpleEvent } from '../../../../shared/core/cqrs/event/simple-event'
 import { AccountCreated } from '../../domain/events/account-created/account-created'
 import { AccountCreatedHandler } from '../../domain/events/account-created/account-created-handler'
 
@@ -12,7 +12,7 @@ export class IamEventBus extends EventBus {
 		}
 	]
 
-	public async send(event: Event<unknown>): Promise<void> {
+	public async send(event: SimpleEvent<unknown>): Promise<void> {
 		console.log(`Sending event: ${JSON.stringify(event)}`)
 		const binding = this.bindings.find(b => b.event.name === event.constructor.name)
 
--- apps/server/src/shared/core/cqrs/event/simple-event.ts
+++ apps/server/src/shared/core/cqrs/event/simple-event.ts
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid'
 import { UniqueId } from '../../indexing/unique-id'
 
-export abstract class Event<T> {
+export abstract class SimpleEvent<T> {
 	public readonly id: UniqueId = nanoid(256)
 	public readonly occurredOn: Date = new Date()
 	constructor(public readonly payload: T) {}
