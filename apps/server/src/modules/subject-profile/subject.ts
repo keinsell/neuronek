f3b5712b1c02f2c31070f5cd1978187f22916ec8
keinsell <keinsell@protonmail.com>
2023-05-17 17:10:01 +0200

    refactor(subject): 👤 boilerplate `Subject` interface (#370)

---
+++ apps/server/src/modules/subject-profile/subject.ts
@@ -0,0 +1,31 @@
+// These data would be used mostly for in-platform sharing and are optional to respect user's privacy, weight,
+// age, and height will be actually used to define personalised dosage.
+// Additionall data will be anonimized and maybe sent to analitics to support public reseach.
+import { NotImplemented } from '../../shared/core/domain/exceptions/not-implemented.js'
+
+export interface Subject {
+	// Subject may be linked to specific account.
+	// MAY because we'll be aggregating experience reports from external sources and we would like to segregate
+	// experiences by subjects which would make research a bit more transparent.
+	accountId?: string
+	displayName?: string
+	firstName?: string
+	lastName?: string
+	birthDate?: Date
+	weight?: number
+	height?: number
+	nationality?: string
+	// TODO: We may think about health conditions, mental disorders or some other data that may be worthly for analitics.
+}
+
+export function createSubject(_payload: Subject): Subject {
+	throw new NotImplemented()
+}
+
+export function updateSubject(_subjectId: string, _payload: Partial<Subject>): Subject {
+	throw new NotImplemented()
+}
+
+export function deleteSubject(_subjectId: string): Promise<void> {
+	throw new NotImplemented()
+}
