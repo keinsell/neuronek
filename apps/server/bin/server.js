afb1883b75400505bfbaf98577cff386150cbe27
keinsell <keinsell@protonmail.com>
2023-05-23 21:47:03 +0200

    docs: 📝 use `tsoa` for openapi documentation (#376)

--- apps/server/bin/server.js
+++ apps/server/bin/server.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-
 import { HttpApplication } from '../dist/index.js'
 
 HttpApplication.listen(Number(process.env.PORT)).on('listening', () => {
 	const url = `${process.env.API_URL}`
-	console.log(`🚀 Server started at ${url}`)
+	console.log(`🚀 Server started at ${url}/docs`)
 })
