23cf238c4cf9e3f5982417c63f270fa52075c6d5
Jakub Olan <keinsell@protonmail.com>
2023-05-24 03:36:18 +0200

    refactor(`EventBus`): 🔧 add long-polling mechanism

--- apps/server/src/shared/common/event-bus/in-memory-event-bus.ts
+++ apps/server/src/shared/common/event-bus/in-memory-event-bus.ts
@@ -1,5 +1,6 @@
 import { EventHandler } from '../../core/cqrs/event/event-handler.js'
 import { IntegrationEvent } from '../../core/cqrs/event/integration-event.js'
+import { UniqueId } from '../../core/indexing/unique-id.js'
 import { EventBus } from './event-bus.js'
 import { EventEmitter } from 'events'
 import type { Constructor } from 'type-fest'
@@ -7,40 +8,53 @@ import type { Constructor } from 'type-fest'
 export class InMemoryEventBus extends EventBus {
 	private bindingStorage: Map<Constructor<IntegrationEvent>, EventHandler>
 	private eventEmitter: EventEmitter
+	private listeners: Set<UniqueId>
 
 	constructor() {
 		super()
 		this.bindingStorage = new Map<Constructor<IntegrationEvent>, EventHandler>()
 		this.eventEmitter = new EventEmitter()
+		this.listeners = new Set()
 	}
 
-	public async handle(command: IntegrationEvent): Promise<void> {
-		const handler = this.bindingStorage.get(command.constructor as Constructor<IntegrationEvent>)
+	public async dispatch(event: IntegrationEvent): Promise<void> {
+		const handler = this.bindingStorage.get(event.constructor as Constructor<IntegrationEvent>)
 
 		if (handler) {
-			return await handler.handle(command)
+			this.eventEmitter.emit(event.constructor.name, event)
+			console.log(`${this.constructor.name} published ${event.constructor.name}`)
 		} else {
-			throw new Error(`No handler registered for command type: ${command.constructor.name}`)
+			throw new Error(`No handler registered for command type: ${event.constructor.name}`)
 		}
 	}
 
 	public registerHandler<T extends IntegrationEvent = IntegrationEvent>(
-		commandConstructor: Constructor<T>,
+		event: Constructor<T>,
 		handler: EventHandler
 	): void {
-		this.bindingStorage.set(commandConstructor, handler)
-		this.eventEmitter.on(commandConstructor.name, async (command: T) => {
-			await this.handle(command)
+		this.bindingStorage.set(event, handler)
+		this.eventEmitter.on(event.name, async (command: T) => {
+			await this.handleSingleEvent(command)
+			this.listeners.delete(command._id)
 		})
 	}
 
-	public async dispatch(command: IntegrationEvent): Promise<void> {
-		const handler = this.bindingStorage.get(command.constructor as Constructor<IntegrationEvent>)
+	async handle(event: IntegrationEvent): Promise<void> {
+		this.listeners.add(event._id)
+		await this.dispatch(event)
+
+		while (this.listeners.has(event._id)) {
+			await new Promise(resolve => setTimeout(resolve, 10))
+		}
+	}
+
+	private async handleSingleEvent(event: IntegrationEvent): Promise<void> {
+		const handler = this.bindingStorage.get(event.constructor as Constructor<IntegrationEvent>)
 
 		if (handler) {
-			this.eventEmitter.emit(command.constructor.name, command)
+			await handler.handle(event)
 		} else {
-			throw new Error(`No handler registered for command type: ${command.constructor.name}`)
+			throw new Error(`No handler registered for command type: ${event.constructor.name}`)
 		}
 	}
 }
