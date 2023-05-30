5ca8c82f1fe2d496bb54ddc12ed7161eeefed90b
Jakub Olan <keinsell@protonmail.com>
2023-05-30 21:36:00 +0200

    ???

--- packages/osiris/src/shared.core/entity.ts
+++ packages/osiris/src/shared.core/entity.ts
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid'
-import { UniqueId } from 'server/src/shared/core/indexing/unique-id'
+import type { UniqueId } from 'server/src/shared/core/indexing/unique-id'
 
 /** Entity represents objects that most likely should be saved into database. This modified version aims to introduce additional property which is `id` for easier manipulating data when used in database. */
 export class Entity {
