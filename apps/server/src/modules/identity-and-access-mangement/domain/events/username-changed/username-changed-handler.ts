6ea01531a1499011a6cc4bd7dccb502feedebc9f
Jakub Olan <keinsell@protonmail.com>
2023-05-30 02:21:02 +0200

    chore: xd

--- apps/server/src/modules/identity-and-access-mangement/domain/events/username-changed/username-changed-handler.ts
+++ apps/server/src/modules/identity-and-access-mangement/domain/events/username-changed/username-changed-handler.ts
@@ -2,8 +2,8 @@ import { AccountWriteRepository } from '../../../infrastructure/repositories/acc
 import { UsernameChanged } from './username-changed.js'
 import { DomainHandler } from '~foundry/domain'
 
-export class UsernameChangedHandler extends DomainHandler {
-	public async execute(event: UsernameChanged): Promise<void> {
+export class UsernameChangedHandler extends DomainHandler<UsernameChanged> {
+	public async handle(event: UsernameChanged): Promise<void> {
 		await new AccountWriteRepository().save(event.account)
 	}
 }
