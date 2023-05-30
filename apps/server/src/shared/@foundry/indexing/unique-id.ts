89ca4f739bb38fc682347b2ff133ebb38b610fc2
Jakub Olan <keinsell@protonmail.com>
2023-05-30 23:39:24 +0200

    refactor: use UniqueId as brand

--- apps/server/src/shared/@foundry/indexing/unique-id.ts
+++ apps/server/src/shared/@foundry/indexing/unique-id.ts
@@ -1,4 +1,8 @@
+import { ULID } from '~foundry/indexing/ulid/index.js'
 import { CUID } from './cuid.js'
 import { SequentialID } from './sequential-id.js'
 
-export type UniqueId = string | number | bigint | SequentialID | CUID
+type Brand<T, BrandName extends string> = T & { __brand: BrandName }
+type UniqueIdBrand = 'UniqueIdBrand'
+
+export type UniqueId = Brand<string | number | bigint | SequentialID | CUID | ULID, UniqueIdBrand>
