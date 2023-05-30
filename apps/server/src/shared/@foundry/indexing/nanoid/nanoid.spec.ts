4845c2c3d34f1ac6715d2a2329cde227fa32088c
keinsell <keinsell@protonmail.com>
2023-05-31 00:40:46 +0200

    feat(`@foundry`): ✨ add `nanoid` identifier (#390)

---
+++ apps/server/src/shared/@foundry/indexing/nanoid/nanoid.spec.ts
@@ -0,0 +1,25 @@
+import test               from 'ava'
+import { InvalidValue }   from '~foundry/exceptions/invalid-value.js'
+import { NanoID, nanoid } from './nanoid.js'
+
+
+
+test('nanoid() generates a valid Nanoid', (t) => {
+	const id = nanoid()
+	t.true(isValidNanoid(id))
+})
+
+test('nanoid(id) returns the provided Nanoid if valid', (t) => {
+	const validNanoid = 'UMmgcRhFekXDAmrtUJYFc'
+	const id = nanoid(validNanoid)
+	t.is(id, validNanoid as NanoID)
+})
+
+test('nanoid() throws InvalidValue error if invalid Nanoid is generated', (t) => {
+	t.throws(() => nanoid('xyz'), { instanceOf: InvalidValue })
+})
+
+function isValidNanoid(id: string): boolean {
+	const nanoidRegex = new RegExp(`^[a-zA-Z0-9_-]{8,64}$`)
+	return nanoidRegex.test(id)
+}
--- apps/server/src/shared/@foundry/indexing/unique-id.ts
+++ apps/server/src/shared/@foundry/indexing/unique-id.ts
@@ -1,8 +1,11 @@
-import { ULID } from '~foundry/indexing/ulid/index.js'
-import { CUID } from './cuid.js'
+import { NanoID }       from '~foundry/indexing/nanoid/index.js'
+import { ULID }         from '~foundry/indexing/ulid/index.js'
+import { CUID }         from './cuid.js'
 import { SequentialID } from './sequential-id.js'
+
+
 
 type Brand<T, BrandName extends string> = T & { __brand: BrandName }
 type UniqueIdBrand = 'UniqueIdBrand'
 
-export type UniqueId = Brand<string | number | bigint | SequentialID | CUID | ULID, UniqueIdBrand>
+export type UniqueId = Brand<string | number | bigint | SequentialID | CUID | ULID | NanoID, UniqueIdBrand>
