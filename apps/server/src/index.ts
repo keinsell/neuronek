9f64bc4d5de04930ebf9ecdb7c0d0f643131ca88
keinsell <keinsell@protonmail.com>
2023-06-02 05:55:13 +0200

    refactor: 💡 swap `Aggregate` with `AggregateRoot` (#391)

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,36 +1,3 @@
 import 'reflect-metadata'
 
 export { HttpApplication } from './interfaces/http/http.js'
-
-//new Identity(
-//	new Account({
-//		username: await createUsername('keinsell'),
-//		password: await createPasswordHash('asdsasddsad')
-//	})
-//)
-//
-//// Send 30 messages by looping
-//
-//class UserCreatedMessageHandler extends MessageHandler<undefined> {
-//	public async handle(message: Message<undefined>): Promise<void> {
-//		console.log(message)
-//	}
-//}
-//
-//const producer = new MemphisMessageProducer('neuronek', cuid())
-//await producer.connect()
-//
-//const consumer = new MemphisMessageConsumer('neuronek', cuid())
-//await consumer.start()
-//await consumer.subscribe('user-created', new UserCreatedMessageHandler())
-//await consumer.subscribe('user-created', new UserCreatedMessageHandler())
-//
-//for (let index = 0; index < 1000; index++) {
-//	const userCreated = new Message({
-//		payload: {
-//			username: nanoid(1024)
-//		},
-//		messageType: 'user-created'
-//	})
-//	producer.send<undefined>(userCreated as any)
-//}
--- apps/server/src/shared/@foundry/domain/aggregate-root.ts
+++ apps/server/src/shared/@foundry/domain/aggregate-root.ts
@@ -1,41 +1,5 @@
-import { UniqueId } from '../indexing/unique-id.js'
-import { Aggregate } from './aggregate.js'
-import { DomainEvent } from './domain-event.js'
-import { Entity } from './enity.js'
+import { Entity } from '~foundry/domain/enity.js'
 
-export abstract class AggregateRoot<T extends Entity | Aggregate> {
-	protected readonly _id: UniqueId | undefined
-	protected root: T
 
-	protected constructor(root: T, version?: number) {
-		this._id = root._id
-		this._version = version || 0
-		this._events = []
-		this.root = root
-	}
 
-	protected _version: number
-
-	public get version(): number {
-		return this._version
-	}
-
-	protected _events: DomainEvent<T>[]
-
-	public get events(): DomainEvent<T>[] {
-		return this._events
-	}
-
-	public addEvent(event: DomainEvent<T>): void {
-		this._events.push(event)
-		this.incrementVersion()
-	}
-
-	public clearEvents(): void {
-		this._events.length = 0
-	}
-
-	private incrementVersion(): void {
-		this._version++
-	}
-}
+export class AggregateRoot extends Entity {}
