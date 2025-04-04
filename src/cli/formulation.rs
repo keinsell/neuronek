bb71267c1b3f16c9f47cd1e0cfb067609b86ff70
keinsell <keinsell@protonmail.com>
2025-04-04 14:00:39 +0200

    feat(formulation): add `CreateFormulation` impl (#543)
    Add support for creating formulations. The most important changes
    include adding transaction handling, creating a formulation, and
    updating the main function to pass the application context.

--- src/cli/formulation.rs
+++ src/cli/formulation.rs
@@ -2,11 +2,13 @@ use clap::Parser;
 use derive_more::From;
 use futures::executor::block_on;
 use futures::prelude::*;
+use sea_orm::TransactionTrait;
 use serde::Serialize;
 
 use crate::cli::Displayable;
 use crate::formulation::ingredient::Ingredient;
-use crate::formulation::{Command as FormulationCommand, Formulation};
+use crate::formulation::{create_formulation, Command as FormulationCommand, Formulation};
+use crate::Application;
 
 impl Displayable for Formulation
 {
@@ -35,15 +37,21 @@ pub struct Command
 	command: FormulationCommand,
 }
 
-pub fn handle(command: Command)
+pub fn handle(command: Command, application: &Application)
 {
 	block_on(async {
+		let transaction = application.database_connection.begin().await.unwrap();
 		match command.command {
-			| FormulationCommand::Create(_) => {}
+			| FormulationCommand::Create(cmd) => {
+				let formulation = create_formulation(&cmd, &transaction).await.unwrap();
+				formulation.display(application.stdout_format.clone());
+			}
 			| FormulationCommand::Update(_) => {}
 			| FormulationCommand::Delete(_) => {}
 			| FormulationCommand::List(_) => {}
 			| FormulationCommand::Get(_) => {}
 		};
+
+		transaction.commit().await.unwrap()
 	})
 }
