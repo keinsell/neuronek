09325f91150889f39d81221b88dbcf542e88ac20
keinsell <keinsell@protonmail.com>
2025-10-24 14:39:59 +0200

    chore: add `CreateIngestion` command
    Bootstrap functionality of command-line application with initial command
    which would parse arguments and print them back to stdout.

--- app/Main.hs
+++ app/Main.hs
@@ -8,23 +8,31 @@ data CommandLineApplication = CommandLineApplication String
 -- | Create new @Ingestion@.
 data CreateIngestion = CreateIngestion {
   compound :: String,
-  dosage :: String,
+  dosage :: String
 }
 
+-- TODO: Is there generic version of defining cli argument parsing?
+-- I would rather have clap_derive-like generic/macro rather than
+-- writing parser to manually construct datatype.
 createIngestionCommand :: Parser CreateIngestion
 createIngestionCommand = CreateIngestion
   <$> strOption (
     long "compound"
     <> short 'c'
     <> metavar "COMPOUND"
-    <> help "Compund to be ingested."
-  )
+    <> help "Compund to be ingested.")
   <*> strOption (
     long "dosage"
     <> short 'd'
     <> metavar "DOSAGE"
-    <> help "Amount of COMPOUND being ingested."
-  )
+    <> help "Amount of COMPOUND being ingested.")
 
-main :: IO CreateIngestion
-main = execParser $ info createIngestionCommand mempty
+main :: IO ()
+main = printIngestion =<< execParser opts
+    where opts = info (createIngestionCommand <**> helper) (
+            fullDesc
+            <> progDesc ""
+            <> header "")
+
+printIngestion :: CreateIngestion -> IO ()
+printIngestion (CreateIngestion compound dosage) = putStrLn $ "Ingestion: " ++ compound ++ " " ++ dosage
